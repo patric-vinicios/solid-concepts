608cf89e0b1f1fd4fdfbe650f31dac5ce9ca1013
Patric Vinicios <psdev7@gmail.com>
2025-04-02 12:14:37 -0300

    feat: add methods to in memory

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -20,6 +20,36 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
         return checkIn
     }
 
+    async findById(id: string) {
+        const checkIn = this.items.find((item) => item.id === id)
+    
+        if (!checkIn) {
+          return null
+        }
+    
+        return checkIn
+    }
+
+    async save(checkIn: CheckIn) {
+        const checkInIndex = this.items.findIndex((item) => item.id === checkIn.id)
+    
+        if (checkInIndex >= 0) {
+          this.items[checkInIndex] = checkIn
+        }
+    
+        return checkIn
+    }
+
+    async countByUserId(userId: string) {
+        return this.items.filter((checkIn) => checkIn.user_id === userId).length
+    }
+
+    async findManyByUserId(userId: string, page: number) {
+        return this.items
+          .filter((checkIn) => checkIn.user_id === userId)
+          .slice((page - 1) * 20, page * 20)
+      }
+
     async findByUserIdOnDate(userId: string, date: Date) {
         const startOfTheDay = dayjs(date).startOf('date')
         const endOfTheDay = dayjs(date).endOf('date')

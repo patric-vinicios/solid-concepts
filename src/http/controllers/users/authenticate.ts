50a0beb3e4e94121c29094d52fd8522addc3e476
Patric Vinicios <psdev7@gmail.com>
2025-04-15 20:11:10 -0300

    feat: add role to token

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -20,17 +20,39 @@ export async function authenticate(request: FastifyRequest, reply: FastifyReply)
         })
 
         const token = await reply.jwtSign(
-            {},
+            {
+              role: user.role,
+            },
             {
               sign: {
                 sub: user.id,
               },
             },
           )
+
+          const refreshToken = await reply.jwtSign(
+            {
+              role: user.role,
+            },
+            {
+              sign: {
+                sub: user.id,
+                expiresIn: '7d',
+              },
+            },
+          )
       
-          return reply.status(200).send({
-            token,
-          })
+          return reply
+            .setCookie('refreshToken', refreshToken, {
+              path: '/',
+              secure: true,
+              sameSite: true,
+              httpOnly: true,
+            })
+            .status(200)
+            .send({
+              token,
+            })
 
     } catch(error) {
         if(error instanceof InvalidCredentialsError) return reply.status(400).send({message: error.message})
